4fb1666d0d61c33cce1b52454bab7cd6d3f43013
Saman Alvi <NinjaDoppleganger@Samans-MacBook-Pro.local>
2015-11-22 07:54:24 -0500

    Saman - styling and added in timing for single text message usage

--- public/app/modal/unicastMessageModal.js
+++ public/app/modal/unicastMessageModal.js
@@ -1,20 +1,19 @@
 angular.module('myApp').controller('unicastMessageModalController', ['$scope', '$uibModalInstance', 'APIService', 'ServiceService', function ($scope, $uibModalInstance, APIService, ServiceService) {
   $scope.items = ['item1', 'item2', 'item3'];
+  var currDate = Date.now();
   $scope.unicastMessage = {
-    timestamp: Date.now()
-  };
-
-  $scope.selected = {
-    item: $scope.items[0]
+    now: currDate,
+    time: 0
   };
 
   $scope.ok = function () {
     $scope.unicastMessage.service = ServiceService.currentService.serviceName;
 
     console.log($scope.unicastMessage);
+    $scope.unicastMessage.timestamp = $scope.unicastMessage.now + $scope.unicastMessage.time * 60000;
     APIService.unicastMessage($scope.unicastMessage)
 
-    $uibModalInstance.close($scope.selected.item);
+    $uibModalInstance.close();
   };
 
   $scope.cancel = function () {
